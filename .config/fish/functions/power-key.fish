da0e30a2b24a9dd17808d7f52e45451f05df618b
Gokul <gokulps15@gmail.com>
2019-04-22 11:23:22 +0530

    fish: change (history)[1] to $history[1]

--- .config/fish/functions/power-key.fish
+++ .config/fish/functions/power-key.fish
@@ -2,7 +2,7 @@ function power-key
         if test (commandline) = ""
                 history merge
         else if test (commandline) = " "
-                commandline -r (history)[1]
+                commandline -r $history[1]
         else
                 commandline -f accept-autosuggestion
         end
--- .config/fish/functions/show-man-page.fish
+++ .config/fish/functions/show-man-page.fish
@@ -1,6 +1,6 @@
 function show-man-page
     if test (commandline) = ""
-        commandline -r (history)[1]
+        commandline -r $history[1]
         __fish_man_page
         commandline -r ""
         commandline -f repaint
