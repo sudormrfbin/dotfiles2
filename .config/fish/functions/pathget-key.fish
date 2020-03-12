342af4fe8dd95a750361b33a7e6b27706c952d65
Gokul <gokulps15@gmail.com>
2020-03-13 03:44:43 +0530

    fish: fix indentation of pathget-key

--- .config/fish/functions/pathget-key.fish
+++ .config/fish/functions/pathget-key.fish
@@ -1,6 +1,6 @@
 function pathget-key
-	if test (commandline) = ""
-commandline -r $history[1]
-end
-commandline -a ' | path-extractor | fzf --bind "enter:execute(vim {+})" --multi'
+    if test (commandline) = ""
+        commandline -r $history[1]
+    end
+    commandline -a ' | path-extractor | fzf --bind "enter:execute(vim {+})" --multi'
 end
