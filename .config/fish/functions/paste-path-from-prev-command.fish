93a4ddc53e8710952fa42548fe873b3224c704d6
Gokul <gokulps15@gmail.com>
2020-03-13 03:44:11 +0530

    fish: fix command not found bug - use eval

--- .config/fish/functions/paste-path-from-prev-command.fish
+++ .config/fish/functions/paste-path-from-prev-command.fish
@@ -1,7 +1,9 @@
 # Defined in /tmp/fish.sD1SCf/paste-path-from-prev-command.fish @ line 2
 function paste-path-from-prev-command
-	set -l file ( $history[1] | path-extractor | fzf --multi)
-    commandline -a "$file "
-    commandline -f end-of-line
+	set -l file ( eval $history[1] | path-extractor | fzf --multi )
+    if test -n "$file"  # quoting is necesssary
+        commandline -a "$file "
+        commandline -f end-of-line
+    end
     commandline -f repaint
 end
