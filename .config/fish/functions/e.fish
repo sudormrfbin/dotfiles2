4fc8cdf35852e2fac388f2a92d3bd866eb54d74a
Gokul <gokulps15@gmail.com>
2020-03-13 21:40:56 +0530

    fish: add description to e function

--- .config/fish/functions/e.fish
+++ .config/fish/functions/e.fish
@@ -1,5 +1,5 @@
 # Defined in /tmp/fish.9yscR3/e.fish @ line 2
-function e
+function e -d 'Edit dotfiles'
 	set -l file (frece print ~/.cache/yadm-files.txt | fzf)
         if test ! $file = ""
                 frece increment /home/gokul/.cache/yadm-files.txt $file
