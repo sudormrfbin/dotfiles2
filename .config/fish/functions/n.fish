041ac256bb8d67536ef12b018f89e5e866c4b703
Gokul <gokulps15@gmail.com>
2020-03-15 00:13:15 +0530

    fish: add n as notes function

---
+++ .config/fish/functions/n.fish
@@ -0,0 +1,11 @@
+function n
+    set -l selection (command ls ~/notes/ | fzf --print-query --preview="cat $HOME/notes/{}")
+    if test $status -eq 130
+        return 1 # exit by esc or ctrl-c
+    end
+    if test -n "$selection[2]" # file exists
+        nvim "$HOME/notes/$selection[2]"
+    else
+        nvim "$HOME/notes/$selection[1].md" # use query as filename
+    end
+end
