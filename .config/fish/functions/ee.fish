58fa171ecc29f68c24eae3497231e2fdda4617ac
Gokul <gokulps15@gmail.com>
2020-03-16 22:02:10 +0530

    fish: sort modified files by time in ee

--- .config/fish/functions/ee.fish
+++ .config/fish/functions/ee.fish
@@ -2,7 +2,7 @@ function ee -d 'Stage changes to dotfiles'
     pushd $HOME
 
     set file (
-                yadm diff --name-only |
+                ls -t (yadm diff --name-only) |
                 fzf --preview="yadm diff --color {}" --height=100% --multi --prompt="Stage: "
             )
     if test -n "$file"
