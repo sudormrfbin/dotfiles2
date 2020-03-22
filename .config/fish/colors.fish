ac1c0d214d50b21a6f94eb6a212d11ed847d949e
Gokul <gokulps15@gmail.com>
2020-03-22 21:12:08 +0530

    fish: save syntax colors to file

---
+++ .config/fish/colors.fish
@@ -0,0 +1,27 @@
+set fish_color_autosuggestion 'brwhite'  '--dim'
+set fish_color_cancel normal
+set fish_color_command 'ffffff'  '-i'
+set fish_color_comment white
+set fish_color_cwd 008000
+set fish_color_cwd_root 800000
+set fish_color_end 50FA7B
+set fish_color_error 'red'  '--reverse'
+set fish_color_escape 00a6b2
+set fish_color_history_current normal
+set fish_color_host normal
+set fish_color_host_remote yellow
+set fish_color_match '-b'  'magenta'
+set fish_color_normal normal
+set fish_color_operator 'ffc0cb'  '--bold'
+set fish_color_param cyan
+set fish_color_quote yellow
+set fish_color_redirection 'purple'  '--bold'
+set fish_color_search_match --background=magenta
+set fish_color_selection --background=purple
+set fish_color_status red
+set fish_color_user 00ff00
+set fish_color_valid_path 'cyan'  '--underline'
+set fish_pager_color_completion normal
+set fish_pager_color_description 'bryellow'  '--bold'
+set fish_pager_color_prefix 'white'  '--bold'  '--underline'
+set fish_pager_color_progress 'brwhite'  '--background=cyan'
