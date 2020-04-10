7b08bfe39718cb4a8895aae0ab743930ec01ecd3
Gokul <gokulps15@gmail.com>
2020-04-10 13:22:23 +0530

    scripts: add braindump.sh to manage short todos

---
+++ .local/bin/braindumb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dmenucmd='dmenu -fn mononoki-14 -l 10'
+
+# https://stackoverflow.com/questions/46131727/printing-multiple-values-on-the-same-line
+# https://stackoverflow.com/questions/28164849/using-jq-to-parse-and-display-multiple-fields-in-a-json-serially
+# https://stackoverflow.com/questions/34834519/how-do-i-select-multiple-fields-in-jq
+
+tasklist=$(task +braindump +READY export | jq -r '.[] | "\(.id) ▪ \(.description)"')
+
+if selected=$(echo "$tasklist" | $dmenucmd)
+then
+    if id=$(echo $selected | grep -oP '^\d+')
+    then
+        # selected an old item; mark as completed
+        task done $id
+    else
+        # typed in a new item; add it to list
+        task add +braindump "$selected"
+    fi
+fi
