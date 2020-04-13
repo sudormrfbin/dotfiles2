35756e07257f472ee23c062fbdd6cb66c13a0937
Gokul <gokulps15@gmail.com>
2020-04-13 21:27:17 +0530

    fish: make yt automaticaly save and play downloaded mp3

--- .config/fish/functions/yt.fish
+++ .config/fish/functions/yt.fish
@@ -1,4 +1,8 @@
-# Defined in - @ line 1
-function yt --wraps='youtube-dl -f bestaudio -x --audio-format mp3 -o "%(title)s.%(ext)s"' --description 'alias yt youtube-dl -f bestaudio -x --audio-format mp3 -o "%(title)s.%(ext)s"'
-  youtube-dl -f bestaudio -x --audio-format mp3 -o "%(title)s.%(ext)s" $argv;
+function yt --wraps='youtube-dl' -d 'download music from youtube in mp3'
+    pushd /mnt/MyStuff/Music/New/NEW/
+    youtube-dl -f bestaudio -x --audio-format mp3 -o "%(title)s.%(ext)s" $argv
+    set -l filename (youtube-dl -f bestaudio -x --audio-format mp3 -o "%(title)s.%(ext)s" --get-filename $argv)
+    set -l filename (echo -s (string split -r -m1 '.' $filename)[1] .mp3)
+    mp3-decoder $filename
+    popd
 end
