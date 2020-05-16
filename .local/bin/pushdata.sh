17b4a392d4e1d33e63c3d58be1ffe650c8c017b7
Gokul <gokulps15@gmail.com>
2020-05-17 00:12:49 +0530

    scripts: move all backups to Backups dir

--- .local/bin/pushdata.sh
+++ .local/bin/pushdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pushd $HOME/gdrive/
-pluck export > Pluckeye/pluckeye.plu
-drive push -quiet -no-prompt Linux\ Notes Bookmarks Pluckeye
+pluck export > Backups/Pluckeye/pluckeye.plu
+drive push -quiet -no-prompt Backups
 popd
 
 yadm push
