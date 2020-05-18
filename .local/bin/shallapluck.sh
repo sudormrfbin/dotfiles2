41d5db1cdfa1f7e0684bf9647ec85b7f729c2016
Gokul <gokulps15@gmail.com>
2020-05-18 20:45:09 +0530

    scripts: add shallapluck.sh

---
+++ .local/bin/shallapluck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download url filter from shallalist.de and add sites to allow
+# and block list of pluckeye
+
+cd /tmp
+
+wget http://www.shallalist.de/Downloads/shallalist.tar.gz
+tar xf shallalist.tar.gz
+
+pluck import-allow BL/education/schools/domains
+pluck import-allow BL/science/astronomy/domains
+pluck import-allow BL/science/chemistry/domains
+
+pluck import-block BL/porn/domains
+pluck import-block BL/spyware/domains
+pluck import-block BL/tracker/domains
