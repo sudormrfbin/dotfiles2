d5b8f0cfeead0ad8cec780c3efe0f5f429af542b
Gokul <gokulps15@gmail.com>
2020-06-02 00:35:45 +0530

    fish: copy previous command if command line is empty with xc

--- .config/fish/functions/xc.fish
+++ .config/fish/functions/xc.fish
@@ -1,7 +1,11 @@
 function xc --description 'Copy piped input or argument to clipboard'
     if isatty stdin
-        # not a pipe or redirection
-        echo -n $argv | xclip -selection clipboard
+        # *not* a pipe or redirection
+        if test (commandline) = ""
+            echo $history[1] | xclip -selection clipboard
+        else
+            echo -n $argv | xclip -selection clipboard
+        end
     else
         cat - | xclip -selection clipboard
     end
