47ea7e82c7ece9f3eaec02c9b88a41584fe67403
Gokul <gokulps15@gmail.com>
2020-06-05 20:59:04 +0530

    fish: yt ffplay autoexit and picard launch

--- .config/fish/functions/yt.fish
+++ .config/fish/functions/yt.fish
@@ -3,6 +3,7 @@ function yt --wraps='youtube-dl' -d 'download music from youtube in mp3'
     youtube-dl -f bestaudio -x --audio-format mp3 -o "%(title)s.%(ext)s" $argv
     set -l filename (youtube-dl -f bestaudio -x --audio-format mp3 -o "%(title)s.%(ext)s" --get-filename $argv)
     set -l filename (echo -s (string split -r -m1 '.' $filename)[1] .mp3)
-    ffplay $filename
+    ffplay -autoexit $filename &
+    picard $filename
     popd
 end
