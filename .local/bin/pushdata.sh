16f5479018cbf42d18cd6f28e06c390cd78dce59
Gokul <gokulps15@gmail.com>
2020-06-13 23:45:25 +0530

    scripts: force overwrite taskwarrior file wduring backup

--- .local/bin/pushdata.sh
+++ .local/bin/pushdata.sh
@@ -4,7 +4,7 @@
 pushd $HOME/gdrive/
 pluck export > Backups/Pluckeye/pluckeye.plu
 gpg -o Backups/fish_history --encrypt -r gokul --yes ~/.local/share/fish/fish_history
-task export | gpg -o Backups/taskwarrior --encrypt -r gokul
+task export | gpg -o Backups/taskwarrior --encrypt -r gokul --yes
 drive push -quiet -no-prompt Backups
 popd
 
