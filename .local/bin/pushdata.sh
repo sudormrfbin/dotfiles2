cb8a327f5a322a4c2db78eee9602a544525c1c91
Gokul <gokulps15@gmail.com>
2020-06-13 22:16:25 +0530

    scripts: encrypt and backup fish history

--- .local/bin/pushdata.sh
+++ .local/bin/pushdata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# deps: pluckeye yadm drive
+# deps: pluckeye yadm drive gpg
 
 pushd $HOME/gdrive/
 pluck export > Backups/Pluckeye/pluckeye.plu
+gpg -o Backups/fish_history --encrypt -r gokul --yes ~/.local/share/fish/fish_history
 drive push -quiet -no-prompt Backups
 popd
 
