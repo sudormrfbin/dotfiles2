1cf628aac1ed168d388f76279efdefda34e9c2bf
Gokul <gokulps15@gmail.com>
2020-06-19 12:20:15 +0530

    fish: cosmetic changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,33 +8,34 @@ end
 
 # Keybindings
 
-set fish_cursor_default block
-set fish_cursor_insert line
+set fish_cursor_default     block
+set fish_cursor_insert      line
 set fish_cursor_replace_one underscore
 
 # Add a way to switch from insert to normal (command) mode.
 # Note if we are paging, we want to stay in insert mode
 # See #2871 in fish-shell github issues
-bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
 bind -M insert -e \e
-bind -M insert \e\x7F backward-kill-word  # alt-backspace
-bind -M insert \e\[P delete-char  # delete key
 
-bind -M insert \eu undo
-bind -M insert \eU redo
-bind -M insert . expand-dot-to-dir
-bind -M insert \ep fish_paginate
-bind -M insert \cx copy-to-clipboard
-bind -M insert \e/ toggle-comment  # Alt-/
-bind -M insert -k f12 show-man-page  # Insert
-bind -M insert "`" power-key
-bind -M insert \e1 forward-word
-bind -M insert \e, up-or-search
-bind -M insert "<>" pathget-key
-bind -M insert "><" paste-path-from-prev-command
-bind -M insert \e3 __fzf_reverse_isearch
-bind -M insert \e- 'commandline -i ~/.config/'
-bind -M insert \eo __fish_preview_current_file
+bind -M insert jk     "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
+bind -M insert \e\x7F backward-kill-word  # alt-backspace
+bind -M insert \e\[P  delete-char  # delete key
+
+bind -M insert \eu    undo
+bind -M insert \eU    redo
+bind -M insert .      expand-dot-to-dir
+bind -M insert \ep    fish_paginate
+bind -M insert \cx    copy-to-clipboard
+bind -M insert \e/    toggle-comment
+bind -M insert -k f12 show-man-page
+bind -M insert "`"    power-key
+bind -M insert \e1    forward-word
+bind -M insert \e,    up-or-search
+bind -M insert "<>"   pathget-key
+bind -M insert "><"   paste-path-from-prev-command
+bind -M insert \e3    __fzf_reverse_isearch
+bind -M insert \e-    'commandline -i ~/.config/'
+bind -M insert \eo    __fish_preview_current_file
 
 # Abbreviations
 
@@ -45,30 +46,30 @@ abbr -a -g fs 'funcsave'
 abbr -a -g wd 'wget -q --show-progress'
 abbr -a -g xc 'xclip -sel clip'
 
-abbr -a -g ga 'git add'
-abbr -a -g gl 'git log'
-abbr -a -g gb 'git branch'
-abbr -a -g gs 'git status'
-abbr -a -g gc 'git commit -m'
-abbr -a -g gd 'git diff --word-diff=color'
-abbr -a -g gco 'git checkout'
-abbr -a -g gsp 'git stash pop'
-abbr -a -g gca 'git commit -am'
+abbr -a -g ga   'git add'
+abbr -a -g gl   'git log'
+abbr -a -g gb   'git branch'
+abbr -a -g gs   'git status'
+abbr -a -g gc   'git commit -m'
+abbr -a -g gd   'git diff --word-diff=color'
+abbr -a -g gco  'git checkout'
+abbr -a -g gsp  'git stash pop'
+abbr -a -g gca  'git commit -am'
 abbr -a -g gcam 'git commit --amend'
-abbr -a -g gss 'git stash push --include-untracked --message'
-abbr -a -g gcl 'git clone --depth=1'
+abbr -a -g gss  'git stash push --include-untracked --message'
+abbr -a -g gcl  'git clone --depth=1'
 abbr -a -g gcls 'git clone --depth=1 --shallow-submodules --recursive'
 
-abbr -a -g pa 'pluck add'
-abbr -a -g pg 'pluck export | grep'
+abbr -a -g pa  'pluck add'
+abbr -a -g pg  'pluck export | grep'
 abbr -a -g pgp 'pluck export | grep \#'
 
-abbr -a -g t 'task'
+abbr -a -g t  'task'
 abbr -a -g ta 'task add'
 abbr -a -g td 'task done'
 abbr -a -g tl 'task list'
 
-abbr -a -g y 'yadm status'
+abbr -a -g y  'yadm status'
 abbr -a -g ya 'yadm add'
 abbr -a -g yd 'yadm diff'
 abbr -a -g yp 'yadm push'
@@ -76,8 +77,8 @@ abbr -a -g yc 'yadm commit -m'
 
 # Variables
 
-set -x EDITOR "nvim"
-set -x PATH ~/.local/bin/ ~/go/bin/ $PATH
+set -x EDITOR  "nvim"
+set -x PATH    ~/.local/bin/ ~/go/bin/ $PATH
 set -x BROWSER /mnt/MyStuff/Downloads/firefox/firefox
 
 set -x Z_CMD "j"
