95224951e7c69259287494a7c0df8d11831517a8
Gokul <gokulps15@gmail.com>
2020-06-22 22:39:30 +0530

    fish: fix jj issue with multiple args

--- .config/fish/functions/jj.fish
+++ .config/fish/functions/jj.fish
@@ -1,6 +1,7 @@
 function jj
-    test -n "$argv";
-        or set -l argv (history search --prefix -n1 'j ' | string split -m1 ' ')[2]
+    test -n "$argv"
+    # split on all spaces because quoted values do not produce results in z
+    or set -l argv (history search --prefix -n1 'j ' | string split ' ')[2..-1]
     # z sends the common directory component to stderr
     cd (j -l $argv 2>/dev/null | tr -s ' ' | cut -f2 -d' ' | fzf)
 end
