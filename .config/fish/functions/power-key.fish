37381272892c9ba9b832f3a3d0670c09cf4dd4ae
Gokul <gokulps15@gmail.com>
2020-06-29 20:09:02 +0530

    fish: fix power key with multiline buffer

--- .config/fish/functions/power-key.fish
+++ .config/fish/functions/power-key.fish
@@ -1,10 +1,11 @@
 function power-key
-    if test (commandline) = "  "
+    set -l cmdline (commandline -b | string collect)
+    if test "$cmdline" = "  "
         history merge
         commandline -r ""
-    else if test (commandline) = ""
+    else if test "$cmdline" = ""
         __fzf_reverse_isearch
-    else if test (commandline) = " "
+    else if test "$cmdline" = " "
         commandline -r $history[1]
     else
         commandline -f accept-autosuggestion
