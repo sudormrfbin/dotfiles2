acb2cdc19e965cca905e1580af505efe362dc0dc
Gokul Soumya <gokulps15@gmail.com>
2020-07-04 12:42:14 +0530

    fish: insert project names with alt-p when using alt-j (taskwarrior)

--- .config/fish/functions/task-id-insert.fish
+++ .config/fish/functions/task-id-insert.fish
@@ -1,10 +1,12 @@
 function task-id-insert
+    set -l fzfcmd 'fzf --bind "alt-p:reload(task _unique project)"'
+    printf $fzfcmd | read -at fzfcmd
     commandline -i (
         task rc.verbose:nothing \
            rc.report.fzf.columns:id,project,description.truncated \
            rc.report.fzf.sort:entry- \
            rc.report.fzf.filter:status:pending fzf |
-        fzf | awk '{ print $1 }'
+        $fzfcmd | awk '{ print $1 }'
     )
     commandline -f repaint
 end
