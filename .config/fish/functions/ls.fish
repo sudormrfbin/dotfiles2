a2ade7e97ca72efee4009ad407e84d7ac8f26ff4
Gokul Soumya <gokulps15@gmail.com>
2021-11-07 15:04:46 +0530

    fish: add ls exa alias

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 1
-function ls --description 'alias ls=ls -hp --color=auto --group-directories-first' -w ls
-    command ls -hp --color=auto --group-directories-first $argv
+# Defined via `source`
+function ls --wraps='exa --icons --classify --color-scale --group-directories-first' --description 'alias ls exa --icons --classify --color-scale --group-directories-first'
+  exa --icons --classify --color-scale --group-directories-first $argv; 
 end
