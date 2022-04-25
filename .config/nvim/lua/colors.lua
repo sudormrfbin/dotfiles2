9f0c30c24b47e4b9c8427707d8e7d5c9ebc82353
Gokul Soumya <gokulps15@gmail.com>
2022-04-25 23:07:17 +0530

    nvim: comment in rgb values for colors

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,19 +1,19 @@
 local theme = {}
 
 theme.onedark = {
-    black       = "#282c34",
-    white       = "#abb2bf",
-    red         = "#e06c75",
-    maroon      = "#be5046",
-    green       = "#98c379",
-    yellow      = "#e5c07b",
-    orange      = "#d19a66",
-    blue        = "#61afef",
-    magenta     = "#c678dd",
-    cyan        = "#56b6c2",
-    grey        = "#5c6370",
-    gutter_grey = "#3f4952",
-    light_grey  = "#2C323C",
+    black       = "#282c34", -- RGB(40, 44, 52)
+    white       = "#abb2bf", -- RGB(171, 178, 191)
+    red         = "#e06c75", -- RGB(224, 108, 117)
+    maroon      = "#be5046", -- RGB(190, 80, 70)
+    green       = "#98c379", -- RGB(152, 195, 121)
+    yellow      = "#e5c07b", -- RGB(229, 192, 123)
+    orange      = "#d19a66", -- RGB(209, 154, 102)
+    blue        = "#61afef", -- RGB(97, 175, 239)
+    magenta     = "#c678dd", -- RGB(198, 120, 221)
+    cyan        = "#56b6c2", -- RGB(86, 182, 194)
+    grey        = "#5c6370", -- RGB(92, 99, 112)
+    gutter_grey = "#3f4952", -- RGB(63, 73, 82)
+    light_grey  = "#2C323C", -- RGB(44, 50, 60)
 }
 
 theme.onedark.fg = theme.onedark.white
