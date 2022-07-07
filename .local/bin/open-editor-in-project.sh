a196d433dd80aeba1db756500cebfd9eaf1d965a
Gokul Soumya <gokulps15@gmail.com>
2022-07-07 20:57:10 +0530

    bspwm: project picker refresh keybind

--- .local/bin/open-editor-in-project.sh
+++ .local/bin/open-editor-in-project.sh
@@ -1,11 +1,67 @@
 #!/usr/bin/env bash
 
-frecedb="${XDG_CACHE_HOME:-$HOME/.cache}/projects-frece.txt"
+FRECE_DB="${XDG_CACHE_HOME:-$HOME/.cache}/projects-frece.txt"
+PROJECTS_DIR="$HOME/Projects"
 
-project="$(frece print $frecedb | fzf)"
+list-projects() {
+    find "$PROJECTS_DIR" -maxdepth 1 -mindepth 1 -type d
+}
 
-# if fzf is cancelled open a shell
-[ $? -ne 0 ] && exec $SHELL
+refresh-projects-list() {
+    frece update --purge-old $FRECE_DB <(list-projects)
+}
 
-project_path="${project/#\~/$HOME}" # expand tilde to HOME
-[ -d "$project_path" ] && frece increment "$frecedb" "$project" && cd "$project_path" && exec $EDITOR
+refresh-projects-list-in-fzf() {
+    refresh-projects-list
+    frece print $FRECE_DB
+}
+
+pick-project() {
+    rebind_cmd="ctrl-r:reload($0 --refresh-projects)"
+    project="$( \
+        frece print $FRECE_DB | \
+        # XXX: THIS WILL BREAK IF $PROJECTS_DIR CHANGES
+        cut -d/ -f5 | \
+        fzf --header 'ctrl-r to reload' \
+            --bind "$rebind_cmd" \
+            # --expect ctrl-e \
+            # ctrl-e signals to edit in helix
+    )"
+
+    # if fzf is cancelled open a shell
+    [ $? -ne 0 ] && exec $SHELL
+
+    # IFS=\n read -r keybind project <<< "$output"
+    # echo $project $editor
+    # [ -z "$keybind" ] && editor=$EDITOR || editor=hx
+
+    project="$PROJECTS_DIR/$project"
+    # open-project "$project" "$editor"
+    open-project "$project"
+
+    # project_path="${project/#\~/$HOME}" # expand tilde to HOME
+}
+
+open-project() {
+    project="$1"
+    editor="${2:-$EDITOR}"
+
+    [ -d "$project" ] && \
+        frece increment "$FRECE_DB" "$project" && \
+        cd "$project" && \
+        exec $EDITOR
+}
+
+subcommand=$1
+case $subcommand in
+    "--refresh-projects")
+        refresh-projects-list-in-fzf
+        ;;
+    "--open-project")
+        open-project "$1" "$2"
+        ;;
+    *)
+        pick-project
+        ;;
+esac
+
